577bc3809e688423c776812dbc0c7a1eb7e7f6c0
Facundo Carballo <carballofacundo70@gmail.com>
2023-12-27 14:59:06 -0300

    problems - starvation

--- problems/starvation.go
+++ problems/starvation.go
@@ -1,1 +1,27 @@
 package problems
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var value = 0
+var mutexValue sync.Mutex
+
+func GoroutineC() {
+	mutexValue.Lock()
+	fmt.Printf("[GoroutineC] value++")
+	value++
+	mutexValue.Unlock()
+}
+
+func Starvation() {
+	mutexValue.Lock()
+	go GoroutineC()
+
+	for {
+		fmt.Printf("Starvation...\n")
+		time.Sleep(time.Second * 3)
+	}
+}
